73f12364be5f632f6249d554c48d4a7752439da1
Francesco <franapoli@gmail.com>
2024-04-14 17:41:22 +0200

    fast version

--- rings.R
+++ rings.R
@@ -5,8 +5,11 @@
 config <- read.table("config.txt")[,2]
 names(config) <- read.table("config.txt")[,1]
 
+
 library(SpatialExperiment)
 load(file.path(config["input_path"], "spe-spots.RData"))
+
+counts_matrix <- read.csv(file.path(config["input_path"], "counts_matrix.csv"), header = T, row.names = 1)
 
 # How many rings?
 hist(spots$distance,
@@ -27,27 +30,43 @@ spots$rings <- rings
 library(parallel)
 
 # Con 10 cpu il calcolo si riduce a 1.62014 mins.
-num_cores <- 10
+num_cores <- as.numeric(config["num_cores"])
 
 # Divido gli indici dei geni in gruppi per parallelizzazione
 gene_groups <- split(1:nrow(counts_matrix), rep(1:num_cores, each = ceiling(nrow(counts_matrix) / num_cores))[1:nrow(counts_matrix)])
 
-# Funzione per calcolare l'espressione media dei geni per ogni ring
-gene_ring_expr_parallel <- function(gene_id, counts_matrix, rings) {
-  ring_expr <- numeric(length(unique(rings)))
+gene_ring_expr_parallel <- function(counts_matrix_row, rings) {
+  urings <- unique(rings)
+  ring_expr <- rep(NA, length(urings))
   
-  for (i in 1:length(unique(rings))) {
-    index_bin <- which(unique(rings)[i] == rings)
-    no_zero <- which(counts_matrix[gene_id, index_bin] != 0)
+  for (i in 1:length(urings)) {
+    index_bin <- urings[i] == rings
+    ring <- counts_matrix_row[index_bin]
+    no_zero <- which(ring != 0)
     
     if (length(no_zero) >= 3) {
-      ring_expr[i] <- sum(counts_matrix[gene_id, index_bin]) / length(no_zero)
-    } else {
-      ring_expr[i] <- 0
+      ring_expr[i] <- sum(counts_matrix_row[index_bin]) / length(no_zero)
     }
   }
   
   return(ring_expr)
+}
+
+
+process_sub_matrix <- function(gene_group) {
+  
+  process_row <- function(counts_matrix_row) {
+    # Filtra i geni con meno di tre counts nella matrice originale
+    if (sum(counts_matrix_row != 0) >= 3) {
+      # Calcola l'espressione media dei geni per ogni ring solo se soddisfa il filtro dei conteggi minimi
+      gene_ring_expr_parallel(counts_matrix_row, rings)
+    } else {
+      rep(0, length(unique(rings)))  # Se non soddisfa il filtro, restituisci un vettore di zeri
+    }
+  }
+  
+  gene_ring_expr_values <- t(apply(counts_matrix[gene_group,], 1, process_row))
+  return(gene_ring_expr_values)
 }
 
 
@@ -56,24 +75,11 @@ calculate_gene_ring_expr_parallel <- function(gene_groups, counts_matrix, rings)
   cluster <- makeCluster(num_cores)
   
   # Esporto la funzione e altre variabili necessarie nell'ambiente di esecuzione parallela
-  clusterExport(cluster, c("gene_ring_expr_parallel", "counts_matrix", "rings"))
   
-  gene_ring_expr_values <- parLapply(cluster, gene_groups, function(gene_indices) {
-    gene_ring_expr_values <- lapply(gene_indices, function(gene_id) {
-      # Filtra i geni con meno di tre counts nella matrice originale
-      if (sum(counts_matrix[gene_id, ] != 0) >= 3) {
-        # Calcola l'espressione media dei geni per ogni ring solo se soddisfa il filtro dei conteggi minimi
-        gene_ring_expr_parallel(gene_id, counts_matrix, rings)
-      } else {
-        rep(0, length(unique(rings)))  # Se non soddisfa il filtro, restituisci un vettore di zeri
-      }
-    })
-    return(gene_ring_expr_values)
-  })
+  
+  clusterExport(cluster, c("gene_ring_expr_parallel", "counts_matrix", "rings", "process_sub_matrix"))
+  gene_ring_expr_values <- parLapply(cluster, gene_groups, process_sub_matrix)
   stopCluster(cluster)
-  
-  # Unisci i risultati in un'unica lista
-  gene_ring_expr_values <- unlist(gene_ring_expr_values)
   
   return(gene_ring_expr_values)
 }
@@ -83,16 +89,20 @@ gene_ring_expr_values <- calculate_gene_ring_expr_parallel(gene_groups, counts_m
 
 
 # Costruisco la matrice matrice_rings
-matrice_rings <- matrix(unlist(gene_ring_expr_values), 
-                        nrow = nrow(counts_matrix),
-                        ncol = 15, 
-                        byrow = TRUE,
-                        dimnames = list(rownames(counts_matrix),
-                                        c("ring1", "ring2", "ring3", "ring4", "ring5",
-                                          "ring6", "ring7", "ring8", "ring9", "ring10",
-                                          "ring11", "ring12", "ring13", "ring14", "ring15")))
+matrice_rings <- do.call(rbind, gene_ring_expr_values)
+dimnames(matrice_rings) <- list(
+  rownames(counts_matrix),
+  paste0("ring", 1:ncol(matrice_rings))
+  )  
 
 # Rimuovo i geni che hanno meno di 5 rings con espressione diversa da zero
 removed_genes <- apply(matrice_rings != 0, 1, sum) < 5
 
+matrice_rings[matrice_rings==0] <- NA
+correlations <- apply(matrice_rings, 1, cor, y=1:ncol(matrice_rings), method="spearman")
+par(mfrow=c(3,3))
+hist(correlations)
+sorter <- order(abs(correlations), decreasing = T)
+for(i in 1:8)
+  plot(matrice_rings[sorter[i], ], xlab="Dist", ylab="Expr", main=rownames(matrice_rings)[i])
 
